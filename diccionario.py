74dc65d07a3955df3af842afc32c84aff5d5a6f5
Joan Cruz <135847775+JoanC9@users.noreply.github.com>
2025-04-10 20:19:41 -0500

    Create diccionario.py

---
+++ diccionario.py
@@ -0,0 +1,13 @@
+diccionario = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            
+            }
+
+palabra = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if palabra in diccionario.keys():
+    
+    # ¿Qué debemos hacer si se encuentra la palabra?
+else:
+    # ¿Qué hacer si no se encuentra la palabra?
